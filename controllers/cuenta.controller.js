522fbbf7e2dfa1837af7f9a6b2306174ded95164
favio-ballon <favioballon@gmail.com>
2024-11-21 14:07:16 -0400

    update: cuenta, funciones nuevas

--- controllers/cuenta.controller.js
+++ controllers/cuenta.controller.js
@@ -3,74 +3,145 @@ const Cuenta = db.cuenta;
 
 // Crear una nueva Cuenta
 exports.crearCuenta = async (req, res) => {
-    try {
-        const data = await Cuenta.create(req.body);
-        res.status(201).send(data);
-    } catch (error) {
-        res.status(500).send({ message: error.message || "Error al crear la cuenta." });
-    }
+  try {
+    const data = await Cuenta.create(req.body);
+    res.status(201).send(data);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || "Error al crear la cuenta." });
+  }
 };
 
 // Obtener todas las Cuentas
 exports.listaCuentas = async (req, res) => {
-    try {
-        const data = await Cuenta.findAll({
-            include: ["cuentaPadre", "cuentasHijas"], // Incluye la relación con Cuenta
-        });
-        res.send(data);
-    } catch (error) {
-        res.status(500).send({ message: error.message || "Error al obtener las cuentas." });
-    }
+  try {
+    const data = await Cuenta.findAll({
+      order: [["codigo", "ASC"]],
+    });
+    res.send(data);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || "Error al obtener las cuentas." });
+  }
 };
 
 // Obtener una Cuenta por ID
 exports.listaCuentaPorID = async (req, res) => {
-    const id = req.params.id;
-    try {
-        const data = await Cuenta.findByPk(id, {
-            include: ["cuentaPadre", "cuentasHijas"], // Incluye la relación con Cuenta
-        });
-        if (!data) return res.status(404).send({ message: "Cuenta no encontrada." });
-        res.send(data);
-    } catch (error) {
-        res.status(500).send({ message: error.message || "Error al obtener la cuenta." });
-    }
+  const id = req.params.id;
+  try {
+    const data = await Cuenta.findByPk(id, {
+      include: ["cuentaPadre", "cuentasHijas"], // Incluye la relación con Cuenta
+    });
+    if (!data)
+      return res.status(404).send({ message: "Cuenta no encontrada." });
+    res.send(data);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || "Error al obtener la cuenta." });
+  }
 };
 
 // Actualizar una Cuenta
 exports.editarCuenta = async (req, res) => {
-    const id = req.params.id;
-    try {
-        const [updated] = await Cuenta.update(req.body, {
-            where: { id },
-        });
-        if (updated) {
-            const updatedCuenta = await Cuenta.findByPk(id);
-            res.send(updatedCuenta);
-        } else {
-            res.status(404).send({ message: "Cuenta no encontrada." });
-        }
-    } catch (error) {
-        res.status(500).send({ message: error.message || "Error al actualizar la cuenta." });
+  const id = req.params.id;
+  try {
+    const [updated] = await Cuenta.update(req.body, {
+      where: { id },
+    });
+    if (updated) {
+      const updatedCuenta = await Cuenta.findByPk(id);
+      res.send(updatedCuenta);
+    } else {
+      res.status(404).send({ message: "Cuenta no encontrada." });
     }
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || "Error al actualizar la cuenta." });
+  }
 };
 
 // Eliminar una Cuenta
 exports.eliminarCuenta = async (req, res) => {
-    const id = req.params.id;
-    try {
-        const deleted = await Cuenta.destroy({
-            where: { id },
-        });
-        if (!deleted) {
-            res.status(404).send({ message: "Cuenta no encontrada." });
-        } else {
-            res.send({ message: "Cuenta eliminada exitosamente." });
-        }
-    } catch (error) {
-        res.status(500).send({ message: error.message || "Error al eliminar la cuenta." });
+  const id = req.params.id;
+  try {
+    const deleted = await Cuenta.destroy({
+      where: { id },
+    });
+    if (!deleted) {
+      res.status(404).send({ message: "Cuenta no encontrada." });
+    } else {
+      res.send({ message: "Cuenta eliminada exitosamente." });
     }
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || "Error al eliminar la cuenta." });
+  }
 };
 
+// Crear cuenta nivel 1, solo se pasa nombre y tipo
+exports.crearCuentaNivel1 = async (req, res) => {
+  try {
+    //get all cuentas nivel 1
+    const cuentas = await Cuenta.findAll({
+      where: {
+        nivel: 1,
+      },
+    });
 
+    //conseguir el ultimo codigo
+    const newCodigo = cuentas.length + 1;
+    req.body.codigo = newCodigo;
 
+    const nuevaCuenta = {
+      codigo: newCodigo,
+      nombre: req.body.nombre,
+      tipo: req.body.tipo,
+      nivel: 1,
+    };
+
+    const data = await Cuenta.create(nuevaCuenta);
+
+    res.status(201).send(data);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || "Error al crear la cuenta." });
+  }
+};
+
+// Crear child cuenta, se pasa nombre, cuentaPadreId
+exports.crearCuentaHija = async (req, res) => {
+  try {
+    const idPadre = req.params.id;
+    //get cuentaPadre with relation cuentasHijas
+    const cuentaPadre = await Cuenta.findByPk(idPadre, {
+      include: ["cuentasHijas"],
+    });
+
+    //conseguir el ultimo codigo
+    const newCodigo = cuentaPadre.cuentasHijas?.length + 1;
+    req.body.codigo = cuentaPadre.codigo + "." + newCodigo;
+    req.body.nivel = cuentaPadre.nivel + 1;
+    req.body.tipo = cuentaPadre.tipo;
+
+    const nuevaCuenta = {
+      codigo: req.body.codigo,
+      nombre: req.body.nombre,
+      tipo: req.body.tipo,
+      nivel: req.body.nivel,
+      padreId: idPadre,
+    };
+
+    const data = await Cuenta.create(nuevaCuenta);
+    res.status(201).send(data);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || "Error al crear la cuenta." });
+  }
+};
--- routes/cuenta.routes.js
+++ routes/cuenta.routes.js
@@ -1,17 +1,21 @@
 const authMiddleware = require("../middlewares/authMiddleware"); // Importar el middleware de autenticación
 
-module.exports = app => {
-    const cuentaController = require("../controllers/cuenta.controller.js");
-    let router = require("express").Router();
+module.exports = (app) => {
+  const cuentaController = require("../controllers/cuenta.controller.js");
+  let router = require("express").Router();
 
-    // Rutas del CRUD protegidas
-    router.post("/", authMiddleware, cuentaController.crearCuenta);
-    router.get("/", authMiddleware, cuentaController.listaCuentas);
-    router.get("/:id", authMiddleware, cuentaController.listaCuentaPorID);
-    router.put("/:id", authMiddleware, cuentaController.editarCuenta);
-    router.delete("/:id", authMiddleware, cuentaController.eliminarCuenta);
+  // Rutas del CRUD protegidas
+  router.post("/", authMiddleware, cuentaController.crearCuenta);
+  router.get("/", authMiddleware, cuentaController.listaCuentas);
+  router.get("/:id", authMiddleware, cuentaController.listaCuentaPorID);
+  router.put("/:id", authMiddleware, cuentaController.editarCuenta);
+  router.delete("/:id", authMiddleware, cuentaController.eliminarCuenta);
 
-    
+  //Crear cuenta nivel 1
+  router.post("/nivel1", authMiddleware, cuentaController.crearCuentaNivel1);
 
-    app.use("/cuentas", router);
-}
+  //Crear cuenta hija
+  router.post("/:id/child", authMiddleware, cuentaController.crearCuentaHija);
+
+  app.use("/cuentas", router);
+};
